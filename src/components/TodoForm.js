1ad6218b5a3210176305823fc4c0189cc96336d0
Adrian Ramirez <96157580+adrianramirez-03@users.noreply.github.com>
2023-03-08 13:11:49 -0800

    Updates

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -2,28 +2,44 @@ import React, { useState } from 'react';
 
 export const TodoForm = ({ addTodo }) => {
   const [value, setValue] = useState('');
+  const [error, setError] = useState(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
+    if (value === '') {
+      setError(true);
+      return;
+    }
+    setError(false);
     addTodo(value);
     setValue('');
   };
 
   return (
-    <form className="TodoForm" onSubmit={handleSubmit}>
-      <input
-        type="text"
-        className="todo-input"
-        placeholder="What is the task today?"
-        value={value}
-        onChange={(e) => {
-          setValue(e.target.value);
-        }}
-      />
-      <button type="submit" className="todo-btn">
-        Add Task
-      </button>
-    </form>
+    <>
+      <form className="TodoForm" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          className={`${error === false ? 'todo-input' : 'todo-input-error'}`}
+          placeholder="What is the task today?"
+          value={value}
+          onChange={(e) => {
+            setValue(e.target.value);
+          }}
+        />
+        <button
+          type="submit"
+          className={`${error === false ? 'todo-btn' : 'todo-btn-error'}`}
+        >
+          Add Task
+        </button>
+        {error ? (
+          <div className="error-msg">Task cannot be empty, try again.</div>
+        ) : (
+          <></>
+        )}
+      </form>
+    </>
   );
 };
